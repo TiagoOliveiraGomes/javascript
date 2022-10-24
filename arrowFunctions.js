320474a61f1234d92294c1fe60099a025abeb511
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-10-24 17:22:12 -0300

    arrow function

---
+++ arrowFunctions.js
@@ -0,0 +1,18 @@
+let double = function (a) {
+    return a * 2
+}
+
+double = (a) => {
+    return a * 2
+}
+
+double = a => a * 2
+
+console.log(double(Math.PI))
+
+let hello = function () {
+    return "Hello"
+}
+
+hello = () => "Hello"
+hello = _ => "Hello"
