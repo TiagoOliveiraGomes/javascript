29816019e3f90cf28cfa9c232fdad554d025808b
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-10-30 15:00:11 -0300

    Create filter2.js

---
+++ filter2.js
@@ -0,0 +1,16 @@
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    
+}
+
+const produtos = [
+    { name: 'Notebook', price: 2499, fragile: true},
+    { name: 'iPad Pro', price: 4199, fragile: true},
+    { name: 'Copo de Vidro', price: 12.49, fragile: true},
+    { name: 'Copo de plástico', price: 18.99, fragile: false},
+]
+
+const expensive = p => p.price >= 500
+const fragile = p => p.fragile
+
+console.log(produtos.filter2(expensive).filter2(fragile))
