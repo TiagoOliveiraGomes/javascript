7e17193548ab3e878b80a4d3c29ae0e8ed244316
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-10-29 22:02:41 -0300

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,6 @@
+const produtos = [
+    { name: 'Notebook', price: 2499, fragil: true},
+    { name: 'iPad Pro', price: 4199, fragil: true},
+    { name: 'Copo de Vidro', price: 12.49, fragil: true},
+    { name: 'Copo de plástico', price: 18.99, fragil: false},
+]
