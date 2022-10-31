b7c7571bd6ffc4b2cc7918e0086fb82fe2d99686
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-10-31 15:28:29 -0300

    Create esNext.js

---
+++ esNext.js
@@ -0,0 +1,19 @@
+{
+    var a = 'Variavel com VAR'
+    let b = 'Variavel com LET'
+    console.log(b)
+}
+
+console.log(a)
+
+const product = 'iPad'
+console.log(`${product} é caro!`)
+
+const [l, e, ...tras] = "Tiago"
+console.log(l, e, tras)
+
+const [x, y] = [1, 2, 3]
+console.log(x, y)
+
+const {nome, year} = {nome:"josé", year: 10}
+console.log(nome, year)
