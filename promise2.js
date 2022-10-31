5d01814a38334a90432b82fffe687730415bf31d
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-10-31 16:33:59 -0300

    Update promise2.js

--- promise2.js
+++ promise2.js
@@ -1,26 +1,40 @@
 const http = require('http')
-const getTurma = (letra, callback) => {
+const getTurma = letra => {
     const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
-    http.get(url, res => {
-        let result = ''
-        res.on('data', dados => {
-            result += dados
-        })
-
-        res.on('end', () => {
-            callback(JSON.parse(result))
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let result = ''
+            res.on('data', dados => {
+                result += dados
+            })
+    
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(result))
+                } catch(e){
+                    reject(e)
+                }
+            })
         })
     })
 }
 
-let nomes = []
-getTurma('A', alunos => {
-    nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
-    getTurma('B', alunos => {
-        nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`))
-        getTurma('C', alunos => {
-            nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`))
-            console.log(nomes)
-        })
-    })
-})
+// let nomes = []
+// getTurma('A').then(alunos => {
+//     nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
+//     getTurma('B').then(alunos => {
+//         nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`))
+//         getTurma('C').then(alunos => {
+//             nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`))
+//             console.log(nomes)
+//         })
+//     })
+// })
+
+Promise.all([getTurma('A'), getTurma('B'), getTurma('C')])
+.then(turmas => [].concat(...turmas))
+.then(alunos => alunos.map(alunos => alunos.nome))
+.then(nomes => console.log(nomes))
+.catch(e => console.log(e))
+
+getTurma('D').catch(e => console.log(e.message))
